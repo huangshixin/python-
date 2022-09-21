15ab2772ae2a79a6c07f4d543b330140a4362e81
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-09-21 09:44:35 +0800

    Update A1-基础知识.py

--- pandas/DataFrame/A1-基础知识.py
+++ pandas/DataFrame/A1-基础知识.py
@@ -1,1 +1,14 @@
+DataFrame
 
+是一个表格型数据结构，可以看作是【由Series组成的字典】，DataFrame由按一定的舒徐排列的多列数据组成；
+
+行索引 index
+列索引：columns
+值：values（numpy二维数组）
+
+dic ={
+ 'userid':Series([]),
+  'username':Series([]),
+  'upaaword':Series([])
+  
+}  
