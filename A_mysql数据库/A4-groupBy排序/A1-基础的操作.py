677a1aa034a1393287de584afb763ce6eb7a6da8
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-09-25 20:50:07 +0800

    Update A1-基础的操作.py

--- A_mysql数据库/A4-groupBy排序/A1-基础的操作.py
+++ A_mysql数据库/A4-groupBy排序/A1-基础的操作.py
@@ -1,1 +1,19 @@
-https://www.bilibili.com/video/BV1iq4y1u7vj?p=22&vd_source=b498949199c494447b72f71d8016104d
+1、排序-------【默认asc】
+  
+  使用 order by 对查询的数据进行排序操作
+  
+  【语法】  order by  ‘key’ desc； 按照key --降序排序
+  【语法】 order by ‘key’ asc； 按照key  升序排序
+  
+SELECT employee_id,last_name,salary
+FROM employees
+ORDER BY salary DESC;
+
+
+
+2、按照列的别名进行排序 ----【在order by中可以使用别名排序】
+
+  
+SELECT employee_id,last_name,salary*12 'key'
+FROM employees
+ORDER BY key DESC;
