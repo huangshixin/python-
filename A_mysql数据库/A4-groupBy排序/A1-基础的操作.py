7b4e13a3b4d8bf012a82c1084cb19a9b74c65840
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-09-25 20:51:58 +0800

    Update A1-基础的操作.py

--- A_mysql数据库/A4-groupBy排序/A1-基础的操作.py
+++ A_mysql数据库/A4-groupBy排序/A1-基础的操作.py
@@ -12,8 +12,16 @@ ORDER BY salary DESC;
 
 
 2、按照列的别名进行排序 ----【在order by中可以使用别名排序】
-
+【ads】别名
   
-SELECT employee_id,last_name,salary*12 'key'
+SELECT employee_id,last_name,salary ads
 FROM employees
-ORDER BY key DESC;
+ORDER BY ads DESC;
+
+
+
+3、order 必须放在 where之后  【如果有where】
+
+
+
+4、order by中的排序内容，不需要是查询的字段；
