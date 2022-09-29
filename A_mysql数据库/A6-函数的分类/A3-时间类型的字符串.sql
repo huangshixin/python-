8cce3924f727084945c8c4d74555f329c2b0bccf
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-09-29 14:26:37 +0800

    Update A3-时间类型的字符串.sql

--- A_mysql数据库/A6-函数的分类/A3-时间类型的字符串.sql
+++ A_mysql数据库/A6-函数的分类/A3-时间类型的字符串.sql
@@ -1,5 +1,5 @@
 [1]
-CURDATE() ，CURRENT_DATE() 返回当前日期，只包含年、月、日
+CURDATE() ，CURRENT_DATE() 返回当前日期，只包含年、月、日 ,now()  【常用 curdate、now】
 CURTIME() ， CURRENT_TIME() 返回当前时间，只包含时、分、秒
 
     SELECT CURDATE() 'time1',CURRENT_DATE() as 'time2',CURTIME() 'time3', CURRENT_TIME() as 'time4';
