a44eb8f6b395129550f14b6485cd3cfdf4203727
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-10-02 10:24:50 +0800

    Update A2-相关子查询案例分析.sql

--- A_mysql数据库/A8-子查询/A2-相关子查询案例分析.sql
+++ A_mysql数据库/A8-子查询/A2-相关子查询案例分析.sql
@@ -28,3 +28,23 @@ where e1.department_id = e2.department_id and e2.salary > e2.avg_val;
 3、可以在where中、from、order by中使用子查询
 
 select\where\order by\ from  中都可以使用子查询
+
+
+
+4、exist  和 not exist 关键字
+exist每次去子查询中找，如果返回True就不找了，否则继续找【目的是查找记录】
+
+not exist 就是当找到一条是False，直接返回；【查询department表中不存在于employees中的记录】
+
+
+select e1.department_id
+from employees e1
+where not exist 
+(
+select * 
+  from employee e2
+  where e1.department_id=e2.department_id
+
+)#相当于拿到e1去e2中去寻找，是否存在e2表中，如果没有就返回
+
+
