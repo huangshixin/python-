f9f642ae3ceea9c351caecc72bb6f60823c7483a
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-10-02 09:43:59 +0800

    Update A1-基础知识.sql

--- A_mysql数据库/A8-子查询/A1-基础知识.sql
+++ A_mysql数据库/A8-子查询/A1-基础知识.sql
@@ -89,6 +89,78 @@ where  e.salary>(select salary from employees where last_name='Abel');
 
 
 ############################################################################################################
+                    
+       #多行子查询
+                    （1）子查询的语句返回多行数据
+                    （2）in：等于列表中的任意一个
+                    （3）any：满足其中一个就行，比如 
+                    （4）all：要求全都都匹配
+                    （5）some：作用与any一样
+      (1)案例
+      select last_name ,employee_id,salary
+      from employees
+      where salary in (
+        select min(salary)
+        from employees
+      ) 
+                    （2）案例：
+                    any/all
+                    返回其它job_id中比job——id为'IT_PROG'部门任一工资低的员工的员工号、姓名、job_id以及salary
+                    
+                    select employee_id,last_name,job_id,salary
+                    from employees 
+                    where job_id<>'IT_PROG'  #这里是筛选 不等于 'IT_PROG'
+                    and  salary < any(
+                          select salary
+                          from employees
+                          where job_id='IT_PROG'
+                          )#查询job_id为it——prog的人的工资 ，然后查询 salary< any（）的值
+                    
+                    （3）案例;在2的基础上查询所有
+                    
+                    select employee_id,last_name,job_id,salary
+                    from employees 
+                    where job_id<>'IT_PROG'  #这里是筛选 不等于 'IT_PROG'
+                    and  salary < all(
+                          select salary
+                          from employees
+                          where job_id='IT_PROG'
+                          )#查询job_id为it——prog的人的工资 ，然后查询 salary< any（）的值
+                    这里查出的工资不是一个值，是一个列表，所以你要使用这个的时候，需要考虑的是all
+                    
+                    
+                    
+                    （4）查询平均工资最低的部门id【mysql聚合函数不能嵌套，orecle可以】
+                    select department_id 
+                    from employees
+                    group by department_id
+                    having AVG(salary) =(
+                    select min(e.salary)
+                     from  (select AVG(salary) avg_sal
+                    from employees
+                    group by department_id))   e#把这个查询结果当作一个表  from（）
+                    
+                    
+                    继续改造
+                    
+                    select department_id
+                    from employees
+                    group by department_id
+                    having AVG(salary) <=ALL(#查询平均工资最低的情况 <=ALL，就可以小于其中最小值
+                          select AVG(salary) 
+                          from employees
+                          group by department_id ）  按照部门的id进行排序，它的平均工资
+                          
+                    
+                                             
+        5、空值问题
+                    如何处理空值id【当内查询中有空值的时候，使用子查询判断语句将会出现空】
+                                             
+                    
+                    
+                    
+                    
+                    
 
 
 
