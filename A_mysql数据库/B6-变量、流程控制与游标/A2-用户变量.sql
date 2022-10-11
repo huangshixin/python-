437c7733d96095ef4bc58056d39f029670d2eb74
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-10-11 10:01:13 +0800

    Update A2-用户变量.sql

--- A_mysql数据库/B6-变量、流程控制与游标/A2-用户变量.sql
+++ A_mysql数据库/B6-变量、流程控制与游标/A2-用户变量.sql
@@ -13,3 +13,30 @@
     #方式2：“:=” 或 INTO关键字
     SELECT @用户变量 := 表达式 [FROM 等子句];
     SELECT 表达式 INTO @用户变量 [FROM 等子句];
+
+        1.2.3 局部变量
+        
+        【定义：可以使用 DECLARE 语句定义一个局部变量】
+        作用域：仅仅在定义它的 BEGIN ... END 中有效
+        位置：只能放在 BEGIN ... END 中，而且只能放在第一句
+        
+        
+        1.定义变量
+        举例：
+        2.变量赋值
+        方式1：一般用于赋简单的值
+        方式2：一般用于赋表中的字段值
+        3.使用变量（查看、比较、运算等）
+        SET @a = 1;
+        SELECT @a;
+        SELECT @num := COUNT(*) FROM employees;
+        SELECT @num;
+        SELECT AVG(salary) INTO @avgsalary FROM employees;
+        SELECT @avgsalary;
+        SELECT @big; #查看某个未声明的变量时，将得到NULL
+
+
+
+
+
+
