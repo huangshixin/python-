94cc332872e1608c0fb0f30c70c993edda9f7c48
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-10-17 10:40:40 +0800

    Create B1-触发器知识.sql

---
+++ A_mysql数据库/B7-触发器/B1-触发器知识.sql
@@ -0,0 +1,36 @@
+触发器 【当某个时间发生的时候，系统会自动的触发】
+
+
+
+1、创建
+
+create trigger 触发器名称
+(before/after) (insert/update/delete) on 表名  【在表前或者表后】
+for each row
+触发器执行的语句块
+
+2.举例实现;
+在
+
+create tirgger tir_name
+before insert employees
+for eacch row
+
+
+
+
+3.
+
+
+
+4.
+
+
+
+
+5.
+
+
+
+
+
