d118dcc84e2e0f34c0148946d58f914657f45738
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-10-21 08:14:53 +0800

    Update A2-SQL大小写规范与sql_model的设置.sql

--- F-MYSQL高级篇/A2-字符集、比较规则、sql规范、目录结构/A2-SQL大小写规范与sql_model的设置.sql
+++ F-MYSQL高级篇/A2-字符集、比较规则、sql规范、目录结构/A2-SQL大小写规范与sql_model的设置.sql
@@ -1,10 +1,25 @@
+sql_model的参数模式不一致
 
 
 
+宽松模式：
+：通过设置宽松模式，来保证大多数sql符合标准的sql语法，在迁移的时候，则不需要对sql业务进行较大 的修改
+
+
+严格模式：
+一般统一使用严格模式；
+
+
+【查看模式】
+
+select @@session.sql_model
+
+select @@global.sql_model
 
 
 
-
+#设置sql_model
+set session sql_model =''
 
 
 
