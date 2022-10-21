3b62aa9adb5f73fbdf0f7b51da3b0951a3d2ff02
huangshixin <38878365+huangshixin@users.noreply.github.com>
2022-10-21 08:49:08 +0800

    Create A3-mysql下的主要数据目录.sql

---
+++ F-MYSQL高级篇/A2-字符集、比较规则、sql规范、目录结构/A3-mysql下的主要数据目录.sql
@@ -0,0 +1,22 @@
+【了解】
+
+cd var/lib/mysql
+
+find / -name mysql
+
+
+数据库文件存放目录
+show variables like 'datadir';
+
+相关命令目录存放路径
+
+/usr/bin/  mysqladmin\mysqlbinlog\mysqldump
+
+
+查看默认数据库
+show databases；
+
+(1) myysql 存放的是做什么的，它存放的是数据
+
+
+
